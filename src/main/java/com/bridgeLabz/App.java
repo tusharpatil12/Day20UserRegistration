8ac5c829217fed70fd88caa5d88f385421154455
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 <tusharpatil22700@gmail.com>
2022-11-12 11:06:02 -0800

    welcome

---
+++ src/main/java/com/bridgeLabz/App.java
@@ -0,0 +1,13 @@
+package com.bridgeLabz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/bridgeLabz/UseRegistration.java
@@ -0,0 +1,17 @@
+package com.bridgeLabz;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class UseRegistration {
+	
+	 @Before
+	    static void displayMethod(){
+	        System.out.println("Welcome to user Registration to junit");
+	    }
+	    @After
+	    public void afterDisplayMethod(){
+	        System.out.println("Test is Passed");
+	    }
+
+}
