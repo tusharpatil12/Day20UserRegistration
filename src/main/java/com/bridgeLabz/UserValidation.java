32c76b9abc2b65a4de556397aab005b060ddbaad
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 <tusharpatil22700@gmail.com>
2022-11-14 13:30:54 -0800

    welcome

--- src/main/java/com/bridgeLabz/UserValidation.java
+++ src/main/java/com/bridgeLabz/UserValidation.java
@@ -5,6 +5,9 @@ import java.util.regex.Pattern;
 public class UserValidation {
 
 	public static final String Validate_Name = "^[A-Z]+[a-z]{2,}$";
+	public static final String EMAIL_ID = "^[A-Z]{1,}[a-z]{5,}[@]{1}[a-z]{2,}[.]?[a-z]{2,}$";
+    public static final String PHONE_NUMBER="^[0-9]+\t[0-9]{10}$";
+
 
 	public Boolean checkFirstName(String firstName) {
 		return (Pattern.matches(Validate_Name, firstName));
@@ -12,8 +15,12 @@ public class UserValidation {
 	public boolean checkLastName(String lastname) {
 		return (Pattern.matches(Validate_Name,lastname));
 	}
-    public Boolean checkEmail(String email_id) {
+	public Boolean checkEmail(String email_id) {
 		return (Pattern.matches(email_id, email_id));
 	}
+	public Boolean checkPhoneNumber(String phone_Number) {
+		return (Pattern.matches(phone_Number,phone_Number));
+	}
+
 
 }
