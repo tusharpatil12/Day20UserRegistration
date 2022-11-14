35c0de68b6da4f0e343943653efdc1d02748171e
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 <tusharpatil22700@gmail.com>
2022-11-14 12:51:48 -0800

    welcome

--- src/test/java/com/bridgeLabz/UseRegistration.java
+++ src/test/java/com/bridgeLabz/UseRegistration.java
@@ -6,21 +6,27 @@ import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class UseRegistration {
-	
-	 @Before
-	   public void displayMethod(){
-	        System.out.println("Welcome to user Registration to junit");
-	    }
-	    @After
-	    public void afterDisplayMethod(){
-	        System.out.println("Test is Passed");
-	    }
-	    @Test
-	    public void given_first_name_should_returnTrue(){
-	        UserValidation validation = new UserValidation();
-	        Boolean result =validation.checkFirstName("Tushar");
-	        Assertions.assertEquals(result,true);
-	 
-	    }
 
+	@Before
+	public void displayMethod(){
+		System.out.println("Welcome to user Registration to junit");
+	}
+	@After
+	public void afterDisplayMethod(){
+		System.out.println("Test is Passed");
+	}
+	@Test
+	public void given_first_name_should_returnTrue(){
+		UserValidation validation = new UserValidation();
+		Boolean result =validation.checkFirstName("Tushar");
+		Assertions.assertEquals(result,true);
+
+	}@Test
+	public void given_last_name_should_returnTrue(){
+		UserValidation validation= new UserValidation();
+		Boolean result1 = validation.checkLastName("Patil");
+		Assertions.assertEquals(result1,true);
+
+	}
+	
 }
