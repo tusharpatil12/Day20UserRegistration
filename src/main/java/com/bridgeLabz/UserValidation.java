f8a70c5f12ec3c3180ba5619a907b035eaefd15c
tusharpatil12 <110172800+tusharpatil12@users.noreply.github.com>
2022-11-13 23:48:35 -0800

    Update UserValidation.java

--- src/main/java/com/bridgeLabz/UserValidation.java
+++ src/main/java/com/bridgeLabz/UserValidation.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 public class UserValidation {
 
 	public static final String Validate_Name = "^[A-Z]+[a-z]{2,}$";
+	public static final String EMAIL_ID = "^[A-Z]{1,}[a-z]{5,}[@]{1}[a-z]{2,}[.]?[a-z]{2,}$";
 
 	public Boolean checkFirstName(String firstName) {
 		return (Pattern.matches(Validate_Name, firstName));
