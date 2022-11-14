5de7f7ae1b922f9bab8a3b43f83ee2c5268cfa56
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 <tusharpatil22700@gmail.com>
2022-11-14 12:39:13 -0800

    welcome

--- src/test/java/com/bridgeLabz/UseRegistration.java
+++ src/test/java/com/bridgeLabz/UseRegistration.java
@@ -2,16 +2,25 @@ package com.bridgeLabz;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class UseRegistration {
 	
 	 @Before
-	    static void displayMethod(){
+	   public void displayMethod(){
 	        System.out.println("Welcome to user Registration to junit");
 	    }
 	    @After
 	    public void afterDisplayMethod(){
 	        System.out.println("Test is Passed");
+	    }
+	    @Test
+	    public void given_first_name_should_returnTrue(){
+	        UserValidation validation = new UserValidation();
+	        Boolean result =validation.checkFirstName("Tushar");
+	        Assertions.assertEquals(result,true);
+	 
 	    }
 
 }
